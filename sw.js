fc73d7bc8068924bd57ecf2769de87a6ff5497cd
saelsa <saelsa@gmx.net>
2018-06-19 15:18:44 +0700

    feat: ServiceWorker added to the site

---
+++ sw.js
@@ -0,0 +1,40 @@
+//Service Worker
+//https://developers.google.com/web/fundamentals/primers/service-workers/
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+    'restaurant.html',
+  'css/styles.css',
+  'data/restaurants.json',
+    'js/main.js',
+
+
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+
