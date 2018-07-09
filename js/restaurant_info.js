fa013cc4e8fcb4fe34e05ae380b479b8919ab78a
saelsa <saelsa@gmx.net>
2018-07-09 10:23:51 +0200

    fix: Included servicerworker for restaurant pages

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -16,6 +16,7 @@ window.initMap = () => {
       });
       fillBreadcrumb();
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
+      registerServiceWorker();
     }
   });
 }
@@ -163,4 +164,20 @@ getParameterByName = (name, url) => {
   if (!results[2])
     return '';
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+//registering a service worker
+//https://developers.google.com/web/fundamentals/primers/service-workers/
+registerServiceWorker = () => {
+  if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+          navigator.serviceWorker.register('sw.js').then(function (registration) {
+              // Registration was successful
+              console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          }, function (err) {
+              // registration failed :(
+              console.log('ServiceWorker registration failed: ', err);
+          });
+      });
+  };
 }
